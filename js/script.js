9b1f4907b7d06c8f01c56b400d023f134481aa61
Tunji Akanbi <you@example.com>
2020-11-19 14:13:34 -0600

    timelines and animations for logo added

--- js/script.js
+++ js/script.js
@@ -36,39 +36,98 @@
         //   $('.navbar-collapse').collapse('hide');
         // });
 
+        //svg DOM elements
         let waves = $('.waves');
         let lakeText = $('#laketime-text');
-        console.log(lakeText);
+        let sunrayChild = $('#sunrays>.cls-2');
+        let seconds = $('#seconds>.cls-5');
+        let hours = $('#hours>.cls-1');
+        let hourmins = $('#hour-mins');
+        console.log(hours);
+
+        //easing variables
         const ei = "power4.in";
         const eo = "power4.out";
         const bo = "bounce.out";
         const bi = "bounce.in";
 
+
         function showWaves() {
           let showwaves = gsap.timeline({
             id: "showwaves"
           });
-          showwaves.fromTo(lakeText, {
-            autoAlpha: 0
-          }, {
-            autoAlpha: 1,
-            duration: 2,
-            ease: ei
-          }, '-=3');
-
           showwaves.fromTo(waves, {
             y: 50,
             autoAlpha: 0
           }, {
             y: 0,
             autoAlpha: 1,
-            duration: 1,
+            duration: 3,
             ease: bo
+          }, "-=2");
+        }
+        function showText(){
+          let showtext = gsap.timeline({
+            id: "showtext"
+          });
+          showtext.fromTo(lakeText, {
+            autoAlpha: 0
+          }, {
+            autoAlpha: 1,
+            duration: 2,
+            ease: ei
           });
         }
 
+        function showRays() {
+          let showrays = gsap.timeline({
+            id: "showrays"
+          });
+          showrays.to('#semi-circ', {
+            autoAlpha: 1,
+            duration:1, 
+            ease: bi
+          });
+          showrays.fromTo(sunrayChild, {
+            autoAlpha: 0,
+            x: 15
+          },{
+            x: 0,
+            autoAlpha: 1,
+            //  duration: 6,
+             stagger: 0.2,
+             ease: ei
+          },"+=3");
+        }
+        function showSeconds(){
+          let showseconds = gsap.timeline({
+            id: "showseconds"
+          });
+          showseconds.to(hours, {autoAlpha: 1, duration: 2},"+=4")
+          showseconds.fromTo(seconds, {
+            autoAlpha: 0,
+            y: 15
+          }, {
+            y: 0,
+            autoAlpha: 1,
+              //duration: 6,
+             stagger: 0.1,
+             ease: ei
+          },"+=3");
+          showseconds.to('#hour-mins', {
+            autoAlpha: 1, 
+            duration: 1, 
+            ease:bi})
+        }
+
         let master = gsap.timeline();
-        master.add(showWaves());
+        master
+        .add(showWaves())
+        .add(showText())
+        .add(showRays())
+        .add(showSeconds())
+        
+        ;
         GSDevTools.create();
         // document ready ends below
         // test code added
